a889c6f71ffaa85a84b74b4161ad048c3533c663
Geraldo Sequeira <gcm.sequeira@gmail.com>
2019-05-10 11:44:40 -0300

    Refactoring tests Users API type request

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -3,22 +3,26 @@ require 'rails_helper'
 RSpec.describe 'Users API', type: :request do
   let!(:user) { create(:user) }
   let(:user_id) { user.id }
+  let(:headers) do
+    {
+      'Accept' => 'apllication/vnd.taskmanager.v1',
+      'Content-Type' => Mime[:json].to_s
+    }
+  end
 
   before { host! 'api.taskmanager.test' }
 
   describe 'GET /users/:id' do 
     before do 
-      headers = { 'Accept' => 'applicaton/vnd.taskmanager.v1' }
       get "/users/#{user_id}", params: {}, headers: headers
     end
 
     context 'when user exists' do 
       it "returns user" do 
-        user_response = JSON.parse(response.body)
-        expect(user_response['id']).to eql(user_id) 
+        expect(json_body[:id]).to eql(user_id) 
       end
 
-      it 'returns http status code 200' do 
+      it 'returns http status code: OK' do 
         expect(response).to have_http_status(200)
       end
     end
@@ -26,7 +30,7 @@ RSpec.describe 'Users API', type: :request do
     context 'when user not exist' do 
       let(:user_id) { 0 }
       
-      it 'returns http status code 404' do 
+      it 'returns http status code: Not Found' do 
         expect(response).to have_http_status(404)
       end
     end
@@ -35,19 +39,17 @@ RSpec.describe 'Users API', type: :request do
 
   describe 'POST /users' do
     before do 
-      headers = { 'Accept' => "apllication/vnd.taskmanager.v1" }
-      post '/users', params: {user: user_params}, headers: headers
+      post '/users', params: { user: user_params }.to_json, headers: headers
     end
     
     context 'when the request params are valid' do
       let(:user_params) { attributes_for(:user) }
       
       it 'return json data for the created user' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:email]).to eq(user_params[:email])
+        expect(json_body[:email]).to eq(user_params[:email])
       end
 
-      it 'return http status code 201' do 
+      it 'return http status code: Created' do 
         expect(response).to have_http_status(201)
       end
     end
@@ -56,11 +58,10 @@ RSpec.describe 'Users API', type: :request do
       let(:user_params) { attributes_for(:user, email: 'john@' ) }
 
       it 'return data json with key error' do 
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response).to have_key(:errors)
+        expect(json_body).to have_key(:errors)
       end
 
-      it 'return http status code 442' do 
+      it 'return http status code: Unprocessable Entity' do 
         expect(response).to have_http_status(442)
       end
 
@@ -70,9 +71,7 @@ RSpec.describe 'Users API', type: :request do
 
   describe 'PUT /users/:id' do
     before do
-      #lembrar de tracar o accept por simbolo
-      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
-      put "/users/#{user_id}", params: { user: user_params }, headers: headers
+      put "/users/#{user_id}", params: { user: user_params }.to_json, headers: headers
     end
     
     context 'when the request parms are valid' do 
@@ -83,15 +82,14 @@ RSpec.describe 'Users API', type: :request do
       end
 
       it 'returns the json data for the updated user' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:email]).to eq(user_params[:email]) 
+        expect(json_body[:email]).to eq(user_params[:email]) 
       end
     end
 
     context 'when the request params are invalid' do
       let(:user_params) { {email: 'jhon@'} } 
 
-      it 'returns http status code Unprocessable Entity' do 
+      it 'returns http status code: Unprocessable Entity' do 
         expect(response).to have_http_status(442)
       end
 
@@ -104,7 +102,6 @@ RSpec.describe 'Users API', type: :request do
 
   describe 'DELETE /users/:id' do
     before do
-      headers = { Accept: 'application/vnd.taskmanager.v1' }
       delete "/users/#{user_id}", params: {}, headers: headers
     end
     
