e328bf9807a7fab17884b6e5dbfb3fbeac289839
Geraldo Sequeira <gcm.sequeira@gmail.com>
2019-05-10 10:10:49 -0300

    now with update action for the users with tes

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -68,4 +68,39 @@ RSpec.describe 'Users API', type: :request do
     
   end
 
+  describe 'PUT /users/:id' do
+    before do
+      #lembrar de tracar o accept por simbolo
+      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+      put "/users/#{user_id}", params: { user: user_params }, headers: headers
+    end
+    
+    context 'when the request parms are valid' do 
+      let(:user_params) { { email: 'e-mail@email.org' } }
+
+      it 'return http status code OK' do 
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the json data for the updated user' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq(user_params[:email]) 
+      end
+    end
+
+    context 'when the request params are invalid' do
+      let(:user_params) { {email: 'jhon@'} } 
+
+      it 'returns http status code Unprocessable Entity' do 
+        expect(response).to have_http_status(442)
+      end
+
+      it 'returns the json data with key errors' do 
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+    end
+  end
+
+  
 end
