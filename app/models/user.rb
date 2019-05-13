b47d8bb725ca37ca6de57bb59cd36d65b60bd1c9
Geraldo Sequeira <gcm.sequeira@gmail.com>
2019-05-13 16:49:49 -0300

    authentication token for user model'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
 
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  before_create :generate_authentication_token!
+  validates_uniqueness_of :auth_token
+  
+
+  def generate_authentication_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while User.exists?(auth_token: auth_token)
+  end
+         
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,25 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:password) }
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   it { is_expected.to allow_value('jhon@example.org').for(:email) }
+  it { is_expected.to validate_uniqueness_of(:auth_token) }
+  
+  describe '#generate_authentication_token!'do
+    it 'generates a unique auth token' do
+      token_default = '1a2b3c4d#'
+      allow(Devise).to receive(:friendly_token).and_return(token_default)
+      user.generate_authentication_token!
+
+      expect(user.auth_token).to eq(token_default)
+    end
+
+    it 'generates another auth token when the current auth token already has been token' do
+      allow(Devise).to receive(:friendly_token).and_return('a12bCyW#', 'a12bCyW#', '!a12bCyW#')
+      old_user = create(:user)
+      user.generate_authentication_token!
+
+      expect(user.auth_token).not_to eq(old_user.auth_token)
+    end
+  end
+  
 
 end
