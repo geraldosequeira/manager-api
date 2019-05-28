e715eb032498ace21d42f68113c9767389497cfb
Geraldo Sequeira <gcm.sequeira@gmail.com>
2019-05-28 09:40:15 -0300

    Refactoring controllers and tests for User for token auth

--- spec/requests/api/v2/users_spec.rb
+++ spec/requests/api/v2/users_spec.rb
@@ -1,26 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe 'Users API', type: :request do 
+  
   let!(:user) { create(:user) }
-  let(:user_id) { user.id }
+  let!(:auth_data) { user.create_new_auth_token }
   let(:headers) do
     {
       'Accept' => 'application/vnd.taskmanager.v2',
-      'Authorization' => "#{user.auth_token}",
-      'Content-Type' => Mime[:json].to_s
+      'Content-Type' => Mime[:json].to_s,
+      'access-token' => auth_data['access-token'],
+      'uid' => auth_data['uid'],
+      'client' => auth_data['client']
     }
   end
 
   before { host! 'api.taskmanager.test' }
 
-  describe 'GET /users/:id' do 
-    before do 
-      get "/users/#{user_id}", params: {}, headers: headers
-    end
+  describe 'GET /auth/validate_token' do 
 
-    context 'when user exists' do 
+    context 'when the request headers are valid' do 
+      before do 
+        get "/auth/validate_token", params: {}, headers: headers
+      end
+      
       it "returns user" do 
-        expect(json_body[:data][:id].to_i).to eql(user_id) 
+        expect(json_body[:data][:id].to_i).to eql(user.id) 
       end
 
       it 'returns http status code: OK' do 
@@ -28,30 +32,33 @@ RSpec.describe 'Users API', type: :request do
       end
     end
 
-    context 'when user not exist' do 
-      let(:user_id) { 0 }
+    context 'when the request headers are not valid' do 
+      before do 
+        headers['access-token'] = 'invalid_token'
+        get '/auth/validate_token', params: {}, headers: headers
+      end
       
-      it 'returns http status code: Not Found' do 
-        expect(response).to have_http_status(404)
+      it 'returns http status code: Unauthorized' do 
+        expect(response).to have_http_status(401)
       end
     end
 
   end
 
-  describe 'POST /users' do
+  describe 'POST /auth' do
     before do 
-      post '/users', params: { user: user_params }.to_json, headers: headers
+      post '/auth', params: user_params.to_json, headers: headers
     end
     
-    context 'when the request params are valid' do
+    context 'when the request params are valid' do 
       let(:user_params) { attributes_for(:user) }
       
       it 'return json data for the created user' do 
-        expect(json_body[:data][:attributes][:email]).to eq(user_params[:email])
+        expect(json_body[:data][:email]).to eq(user_params[:email])
       end
 
-      it 'return http status code: Created' do 
-        expect(response).to have_http_status(201)
+      it 'return http status code: OK' do 
+        expect(response).to have_http_status(200)
       end
     end
 
@@ -63,23 +70,23 @@ RSpec.describe 'Users API', type: :request do
       end
 
       it 'return http status code: Unprocessable Entity' do 
-        expect(response).to have_http_status(442)
+        expect(response).to have_http_status(422)
       end
 
     end
     
   end
 
-  describe 'PUT /users/:id' do
+  describe 'PUT /auth' do 
     before do
-      put "/users/#{user_id}", params: { user: user_params }.to_json, headers: headers
+      put "/auth", params: user_params.to_json, headers: headers
     end
     
-    context 'when the request parms are valid' do
+    context 'when the request parms are valid' do 
       let(:user_params) { { email: 'e-mail@email.org' } }
 
       it 'returns the json data for the updated user' do 
-        expect(json_body[:data][:attributes][:email]).to eq(user_params[:email]) 
+        expect(json_body[:data][:email]).to eq(user_params[:email]) 
       end
 
       it 'return http status code: OK' do 
@@ -87,31 +94,30 @@ RSpec.describe 'Users API', type: :request do
       end
     end
 
-    context 'when the request params are invalid' do
+    context 'when the request params are invalid' do 
       let(:user_params) { {email: 'jhon@'} } 
 
       it 'returns http status code: Unprocessable Entity' do 
-        expect(response).to have_http_status(442)
+        expect(response).to have_http_status(422)
       end
 
       it 'returns the json data with key errors' do 
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response).to have_key(:errors)
+        expect(json_body).to have_key(:errors)
       end
     end
   end
 
-  describe 'DELETE /users/:id' do
+  describe 'DELETE /auth' do 
     before do
-      delete "/users/#{user_id}", params: {}, headers: headers
+      delete "/auth", params: {}, headers: headers
     end
     
-    it 'returns http status code: No Content' do 
-      expect(response).to have_http_status(204)
+    it 'returns http status code: OK' do 
+      expect(response).to have_http_status(200)
     end
 
     it 'No find user in database' do 
-      expect(User.find_by(id: user_id)).to be_nil
+      expect(User.find_by(id: user.id)).to be_nil
     end
   end
 
